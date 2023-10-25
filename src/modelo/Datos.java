17983ffeb3165404ef1ef90aa60050740815fdef
Eliezer Selfin <eliezer.selfin@gmail.com>
2023-10-25 17:55:53 +0200

    primera subida Eli

--- src/modelo/Datos.java
+++ src/modelo/Datos.java
@@ -11,4 +11,34 @@ public class Datos {
 		listaPedidos = new ListaPedidos ();
 	}
 
+	public void setListaArticulos(ListaArticulos listaArticulos) {
+		this.listaArticulos = listaArticulos;
+	}
+
+	public void setListaClientes(ListaClientes listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+
+	public void setListaPedidos(ListaPedidos listaPedidos) {
+		this.listaPedidos = listaPedidos;
+	}
+
+	public Datos(ListaArticulos listaArticulos, ListaClientes listaClientes, ListaPedidos listaPedidos) {
+		this.listaArticulos = listaArticulos;
+		this.listaClientes = listaClientes;
+		this.listaPedidos = listaPedidos;
+	}
+
+	public ListaArticulos getListaArticulos() {
+		return listaArticulos;
+	}
+
+	public ListaClientes getListaClientes() {
+		return listaClientes;
+	}
+
+	public ListaPedidos getListaPedidos() {
+		return listaPedidos;
+	}
+
 }
